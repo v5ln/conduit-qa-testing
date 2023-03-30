4087903474286f9933d84de5738e0cb8767d4c61
OMAR <v5ln@icloud.com>
2023-03-30 20:56:46 +0200

    init the project, and setup the config

---
+++ cypress/support/commands.js
@@ -0,0 +1,6 @@
+// Cypress.Commands.add('login', (email, password) => { 
+//     cy.visit('/login')
+//     cy.get('input[type="email"]').clear().type(email)
+//     cy.get('input[type="password"]').clear().type(password)
+//     cy.get('button[type="submit"]').click()
+// })
